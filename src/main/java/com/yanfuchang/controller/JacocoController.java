2517544560f74ef215a6850d9aaa05f8f9de44b2
yanfuchang <yanfuchang@tujia.com>
2021-09-18 19:08:45 +0800

    1

--- src/main/java/com/yanfuchang/controller/JacocoController.java
+++ src/main/java/com/yanfuchang/controller/JacocoController.java
@@ -27,4 +27,35 @@ public class JacocoController {
         }
     }
 
+
+    @ResponseBody
+    @RequestMapping(value = "/test2", method=RequestMethod.POST, consumes = "application/json")
+    public String jacoco2(
+            @RequestBody Jacoco jacoco
+    ){
+        System.out.println("1");
+        if ("daling2".equals(jacoco.getData())) {
+            return "daling2";
+        }else if ("dalingjia2".equals(jacoco.getData())){
+            return "dalingjia2";
+        }else{
+            return "222";
+        }
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/test3", method=RequestMethod.POST, consumes = "application/json")
+    public String jacoco3(
+            @RequestBody Jacoco jacoco
+    ){
+        System.out.println("2");
+        if ("daling3".equals(jacoco.getData())) {
+            return "daling3";
+        }else if ("dalingjia3".equals(jacoco.getData())){
+            return "dalingjia3";
+        }else{
+            return "333";
+        }
+    }
 }
